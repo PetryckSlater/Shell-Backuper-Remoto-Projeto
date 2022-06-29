51ac6be26cd591dad1d62dbaa6cbcaae13d0b4ba
PetryckSlater <104164472+PetryckSlater@users.noreply.github.com>
2022-06-29 20:59:49 -0300

    Kart

--- index.sh
+++ index.sh
@@ -2,33 +2,40 @@
 
 function config {
 	#definir pasta onde sera feito o backup
+	echo "---------------------------------------------------"
 	echo "digite a pasta que ele deseja fazer o backup: "
 	read loca
+	echo "---------------------------------------------------"
 	echo "ele se localiza na pasta [ $loca ] "
 	#Ip
+	echo "---------------------------------------------------"
 	echo "Digite seu ip: "
 	read ipp
 	ping -c 1 $ipp
 	echo "O ip selecionado foi: $ipp"
+	echo "---------------------------------------------------"
 	
 	#senha
+	echo "---------------------------------------------------"
 	echo "Digite sua senha: "
 	read -s pass
 	echo " Não vacile! "
-	
+	echo "---------------------------------------------------"
 	#user
+	echo "---------------------------------------------------"
 	echo "digite seu usuario: "
 	read user
 	echo "usuario $user configurado..."
 	nome=$user$ipp
 	mkdir $user$ipp
+	echo "---------------------------------------------------"
 
 	#definidor
-	touch configs.txt
-	mv configs.txt config
+	echo "---------------------------------------------------"
 	echo "Arquivo Configs criadas"
 	echo "$user $pass $ipp save_backup/$loca" >> config/configs.txt
 	arb=$(pwd)
+	echo "---------------------------------------------------"
 	#Enviar as paginas
 	echo " Digite a pasta onde o backup do arquivo ficara: "
 	echo "A pasta remota sera: $arb "
@@ -39,10 +46,16 @@ function config {
 	#comprimidor
 	sleep 5
 	echo "comprimindo..." 
+	echo "-"
+	echo "--"
+	echo "---"
+	echo "----"
 	zip -r $arquivo_final.zip $nome
 	sleep 4
 	mv $arquivo_final.zip save_backup
+	echo "---------------------------------------------------"
 	echo "arquivo $arquivo_final movido."
+	echo "---------------------------------------------------"
 	ls -a save_backup/
 	sleep 2
 	rm -rf $nome
@@ -59,7 +72,9 @@ function logfix {
 }
 
 function deub.o {
-	echo -e "Iae deu b.o em que? \n1)Cria a pasta config \n2)Cria a pasta save_backup \n3)Arquivo log criado "
+	echo "---------------------------------------------------"
+	echo -e "Iae deu b.o em que? Caso deseje instalar as pastas digite 4 caso não escolha entre: \n1)Cria a pasta config \n2)Cria a pasta save_backup \n3)Arquivo log criado "
+	echo "---------------------------------------------------"
 	read b_o
 	#Criar o config
 	if [ $b_o -eq 1 ]; then
@@ -90,6 +105,19 @@ function deub.o {
 	echo "Arquivo log esta sendo criado..."
 	sleep 3
 	echo "Arquivo Criado com sucessor"
+	#Instalador das pastas
+	elif [ $b_o -eq 4 ]
+	then
+	echo "Construindo pastas padrão!"
+	mkdir config
+	echo "pasta config criada.."
+	mkdir save_backup
+	echo "pasta save_backup criada.."
+	touch config.txt
+	echo "arquivo config criado.."
+	mv config.txt config
+	echo "Arquivo log.txt criado"
+	touch log.txt
 	#sair
 	else 
 	return 0
